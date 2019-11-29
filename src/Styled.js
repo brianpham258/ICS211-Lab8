02491501bbadb8d2806ab22d125db271a442aa45
Quang Pham <quanghuy25899@gmail.com>
2019-11-29 18:32:33 +0000

    add tag-based styled components

---
+++ src/Styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledForm = styled.form `
+    background-color: papayawhip;
+    box-shadow: 0 0 5px gray;
+    padding: 10px;
+    width: 300px;
+`
