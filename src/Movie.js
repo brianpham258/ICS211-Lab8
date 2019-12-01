d3971ef68d54952d051f1d18dffff7d5cddc9aa3
Quang Pham <quanghuy25899@gmail.com>
2019-12-01 07:03:41 +0000

    modified Title.js and Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 
 const Movie = ({ className, name, link, color, checked }) => {
     if(checked === true) {
---
+++ src/Title.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Title = ({ className, title, subTitle }) => {
+    return (
+        <div className = {className}>
+            <h1>{title}</h1>
+            <h3>{subTitle}</h3>
+        </div>
+    );
+}
+
+const StyledTitle = styled(Title)`
+color: #009900;
+padding: 0 0 0 20px;
+font-family: monospace;
+margin: 0 0 20px 0;
+`;
+
+export default StyledTitle;
