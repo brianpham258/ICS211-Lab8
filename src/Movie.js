149272cc6cae5f19a8a4c47055b0f4c697e686d3
Quang Pham <quanghuy25899@gmail.com>
2019-12-02 19:27:01 +0000

    modified lab8

--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Movie = ({ className, name, link, color, checked }) => {
     if(checked === true) {
--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import StyledMovieList from './MovieList';
 
 const MovieForm = () => {
     const [checkboxGroup, setCheckboxGroup] = React.useState([false, false, false]);
